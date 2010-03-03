6d4855580935f78a3d3f1637a91f7db4c03cfdeb
alee <alee@redhat.com>
2010-03-03 17:35:23 +0000

    Bugzilla Bug 545935 - Add new client-auth ee port to address CVE-2009-3555 TLS: MITM attacks via session renegotiation

--- pki/base/common/src/com/netscape/cms/servlet/csadmin/UpdateDomainXML.java
+++ pki/base/common/src/com/netscape/cms/servlet/csadmin/UpdateDomainXML.java
@@ -246,6 +246,7 @@ public class UpdateDomainXML extends CMSServlet {
         String sport = httpReq.getParameter("sport");
         String agentsport = httpReq.getParameter("agentsport");
         String adminsport = httpReq.getParameter("adminsport");
+        String eecaport = httpReq.getParameter("eeclientauthsport");
         String httpport = httpReq.getParameter("httpport");
         String domainmgr = httpReq.getParameter("dm");
         String clone = httpReq.getParameter("clone");
@@ -313,6 +314,9 @@ public class UpdateDomainXML extends CMSServlet {
             }
             if ((httpport != null) && (!httpport.equals(""))) {
                 attrs.add(new LDAPAttribute("UnSecurePort", httpport));
+            }
+            if ((eecaport != null) && (!eecaport.equals(""))) {
+                attrs.add(new LDAPAttribute("SecureEEClientAuthPort", eecaport));
             }
             if ((domainmgr != null) && (!domainmgr.equals(""))) {
                 attrs.add(new LDAPAttribute("DomainManager", domainmgr));
@@ -382,6 +386,7 @@ public class UpdateDomainXML extends CMSServlet {
                     parser.addItemToContainer(parent, "SecureAgentPort", agentsport);
                     parser.addItemToContainer(parent, "SecureAdminPort", adminsport);
                     parser.addItemToContainer(parent, "UnSecurePort", httpport);
+                    parser.addItemToContainer(parent, "SecureEEClientAuthPort", eecaport);
                     parser.addItemToContainer(parent, "DomainManager", domainmgr);
                     parser.addItemToContainer(parent, "Clone", clone);
                     count ++;
