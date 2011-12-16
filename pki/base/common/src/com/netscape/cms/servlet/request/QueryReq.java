6c21c2fe8887c25260a311aa3fc2934d4af7ee23
awnuk <awnuk@redhat.com>
2011-12-16 22:47:16 +0000

    Fixed bugzilla bug #758505.

--- pki/base/common/src/com/netscape/cms/servlet/request/QueryReq.java
+++ pki/base/common/src/com/netscape/cms/servlet/request/QueryReq.java
@@ -297,7 +297,8 @@ public class QueryReq extends CMSServlet {
     	}
     	
     	
-    	int top=0, bottom=0;
+    	BigInteger top = BigInteger.ZERO;
+    	BigInteger bottom = BigInteger.ZERO;
     	
     	try {
     		String top_s = req.getParameter(OUT_FIRST_ENTRY_ON_PAGE);
@@ -307,14 +308,14 @@ public class QueryReq extends CMSServlet {
     		if (bottom_s == null) bottom_s = "0";
     		
     		if (top_s.trim().startsWith("0x")) {
-    			top = Integer.parseInt(top_s.trim().substring(2), 16);
+    			top = new BigInteger(top_s.trim().substring(2), 16);
     		} else {
-    			top = Integer.parseInt(top_s.trim());
+    			top = new BigInteger(top_s.trim());
     		}
     		if (bottom_s.trim().startsWith("0x")) {
-    			bottom = Integer.parseInt(bottom_s.trim().substring(2), 16);
+    			bottom = new BigInteger(bottom_s.trim().substring(2), 16);
     		} else {
-    			bottom = Integer.parseInt(bottom_s.trim());
+    			bottom = new BigInteger(bottom_s.trim());
     		}
     		
     	} catch (NumberFormatException e) {
@@ -375,19 +376,21 @@ public class QueryReq extends CMSServlet {
      */
     
     private CMSTemplateParams doSearch(Locale l, String filter,
-    		int count, String direction, int top, int bottom)
+    		int count, String direction, BigInteger top, BigInteger bottom)
     {
     	CMSTemplateParams ctp = null;
     	if (direction.equals("previous")) {
-    		ctp = doSearch(l, filter, -count, top-1);
+    		top = top.subtract(BigInteger.ONE);
+    		ctp = doSearch(l, filter, -count, top);
     	} else if (direction.equals("next")) {
-    		ctp = doSearch(l,filter, count, bottom+1);
+    		bottom = bottom.add(BigInteger.ONE);
+    		ctp = doSearch(l,filter, count, bottom);
     	} else if (direction.equals("begin")) {
-    		ctp = doSearch(l,filter, count, 0);
+    		ctp = doSearch(l,filter, count, BigInteger.ZERO);
     	} else if (direction.equals("first")) {
     		ctp = doSearch(l,filter, count, bottom);
     	} else {  // if 'direction is 'end', default here
-    		ctp = doSearch(l,filter, -count, -1);
+    		ctp = doSearch(l,filter, -count, BigInteger.ONE.negate());
     	}
     	return ctp;
     }
@@ -407,7 +410,7 @@ public class QueryReq extends CMSServlet {
     		Locale locale,
     		String filter,
     		int count, 
-    		int marker) {
+    		BigInteger marker) {
     	
     	IArgBlock header = CMS.createArgBlock();
     	IArgBlock context = CMS.createArgBlock();
@@ -423,12 +426,12 @@ public class QueryReq extends CMSServlet {
     		
     		
     		boolean jumptoend = false;
-    		if (marker == -1) {
-    			marker = 0;       // I think this is inconsequential
+    		if (marker.toString().equals("-1")) {
+    			marker = BigInteger.ZERO;       // I think this is inconsequential
     			jumptoend = true; // override  to '99' during search 
     		}
     		
-    		RequestId id = new RequestId(Integer.toString(marker));
+    		RequestId id = new RequestId(marker.toString());
     		IRequestVirtualList list =   mQueue.getPagedRequestsByFilter(
     				id,
     				jumptoend,
@@ -448,8 +451,9 @@ public class QueryReq extends CMSServlet {
     		
     		
     		int currentCount = 0;
-    		int curNum = 0;
-    		int firstNum = -1;
+    		BigInteger curNum = BigInteger.ZERO;
+    		BigInteger firstNum = BigInteger.ONE.negate();
+
     		Enumeration requests = v.elements();
     		
     		while (requests.hasMoreElements()) {
@@ -465,10 +469,10 @@ public class QueryReq extends CMSServlet {
     				continue;
     			}
     			
-    			curNum = Integer.parseInt(
+    			curNum = new BigInteger(
     					request.getRequestId().toString());
     			
-    			if (firstNum == -1) {
+    			if (firstNum.equals(BigInteger.ONE.negate())) {
     				firstNum = curNum; 
     			}
     			
@@ -484,8 +488,8 @@ public class QueryReq extends CMSServlet {
     		
     		header.addIntegerValue(OUT_CURRENTCOUNT, currentCount);
     		header.addStringValue("time", Long.toString(endTime - startTime));
-    		header.addIntegerValue(OUT_FIRST_ENTRY_ON_PAGE, firstNum);
-    		header.addIntegerValue(OUT_LAST_ENTRY_ON_PAGE, curNum);
+    		header.addBigIntegerValue(OUT_FIRST_ENTRY_ON_PAGE, firstNum, 10);
+    		header.addBigIntegerValue(OUT_LAST_ENTRY_ON_PAGE, curNum, 10);
     		
     	} catch (EBaseException e) {
     		header.addStringValue(OUT_ERROR, e.toString(locale));
@@ -539,12 +543,13 @@ public class QueryReq extends CMSServlet {
      */
     private Vector normalizeOrder(Vector list) {
     	
-    	int firstrequestnum = Integer.parseInt(((IRequest) list.elementAt(0))
+    	BigInteger firstrequestnum = new BigInteger(((IRequest) list.elementAt(0))
     			.getRequestId().toString());
-    	int lastrequestnum = Integer.parseInt(((IRequest) list.elementAt(list
+    	BigInteger lastrequestnum = new BigInteger(((IRequest) list.elementAt(list
     			.size() - 1)).getRequestId().toString());
+
     	boolean reverse = false;
-    	if (firstrequestnum > lastrequestnum) {
+    	if (firstrequestnum.compareTo(lastrequestnum) > 0){
     		reverse = true; // if the order is backwards, place items at the beginning
     	}
     	Vector v = new Vector();
